7a6f2601ed311db5b83170e8bebd524726dd6cb5
RAKESHREDDYMAGAM <rakeshreddymagam@gmail.com>
2025-03-09 23:17:44 +0530

    Create fibo series.c

---
+++ fibo series.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+   
+    // int num1;
+    // int num2;
+    // int num3;
+    // int num4;
+    // int num5;
+    // printf("enter a number1 :");
+    // scanf("%d",&num1);
+    // printf("enter a number2 :");
+    // scanf("%d",&num2);
+    
+    
+    // num3= num1+num2;
+    // num4= num3+num2;
+    // num5= num4+num3;
+    // printf( "%d ,%d, %d, %d, %d",num1,num2,num3,num4,num5);
+    int x=0;
+    int y=1;
+    int z=x+y;
+    
+    printf("%d",x);
+    printf("%d",y);
+    printf("%d",z);
+    x=y;
+    y=z;
+    z=x+y;
+     printf("%d",z);
+    
+    return 0;
+}
+
